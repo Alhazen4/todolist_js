597f0a7743253a896ad11d502fdc7f0829f41f5d
Alhazen4 <cybernonk404@gmail.com>
2022-05-31 08:38:04 +0700

    Database failed

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php
-    $db = mysqli_connect('localhost','root','root','todolist') or die ('Unable to connect!');
-    mysqli_connect_error();
+    // $db = mysqli_connect('localhost','root','root','todolist') or die ('Unable to connect!');
+    // mysqli_connect_error();
 ?>
--- input.php
+++ input.php
@@ -1,14 +1,16 @@
 <?php
 
-    include('conn.php');
+    // include('conn.php');
     
-    $input = $_GET['input'];
-    var_dump($input);
+    // $input = $_GET['input'];
+    
+    // $query = "INSERT INTO act_list VALUES('$input')";
+    // $result = mysqli_query($db, $query);
 
-    if(isset($input))
-    {
-        
-        $query = "INSERT INTO act_list VALUES('$input')";
-        $result = mysqli_query($db, $query);
-    }
+    // $validation = mysqli_affected_rows($db);
+
+    // if ($validation > 0)
+    // {
+    //     echo "<script> window.location.href='index.html'; </script>";
+    // }
 ?>
